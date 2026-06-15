87f8bbef4da82bd022cee2709ed0b2c0f2c130ad
denis0134-cell <denis0134@gmail.com>
2026-06-16 01:55:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,14 +18,20 @@ app.get('/webhook', (req, res) => {
 
 app.post('/webhook', async (req, res) => {
   res.sendStatus(200);
-  const { messages } = req.body;
-  if (messages?.length) {
-    for (const msg of messages) {
-      if (msg.type === 'text') {
-        try { await handleIncomingMessage(msg); }
-        catch (err) { console.error('[Server] Error:', err); }
+  try {
+    const messages = req.body?.entry?.[0]?.changes?.[0]?.value?.messages;
+    if (messages?.length) {
+      for (const msg of messages) {
+        if (msg.type === 'text') {
+          await handleIncomingMessage({
+            from: msg.from,
+            text: { body: msg.text.body }
+          });
+        }
       }
     }
+  } catch (err) {
+    console.error('[Server] Error:', err);
   }
 });
 
